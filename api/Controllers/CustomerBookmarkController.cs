0fe15c4595d571aa62be0115c650510d04c06937
dwalker0311 <dwalker32@murraystate.edu>
2022-04-05 18:56:56 -0500

    API and Database
    API and Database added. 2 new tables added

---
+++ api/Controllers/CustomerBookmarkController.cs
@@ -0,0 +1,52 @@
+﻿using Logging.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Logging.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerBookmarkController : ControllerBase
+    {
+        private CustomerBookmarkRepository _logRepo = new CustomerBookmarkRepository();
+        private ILogger<CustomerBookmarkController> _logger;
+
+        public CustomerBookmarkController(ILogger<CustomerBookmarkController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("AddCustomerBookmark")]
+        public IActionResult AddCustomerBookmark([FromBody] CustomerBookmark model)
+        {
+            try
+            {
+                _logRepo.SaveModel(model);
+                return Ok(model);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteCustomerBookmark")]
+        public IActionResult DeleteCustomerBookmark(int bookmarkId)
+        {
+            try
+            {
+                _logRepo.DeleteModel(bookmarkId);
+                return Ok(true);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ api/CustomerLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Logging.API
+{
+    [Table("CustomerLogs")]
+    public partial class CustomerLog
+    {
+        public int LogID { get; set; }
+        public string Application { get; set; }
+        public string ApplicationVersion { get; set; }
+        public int? CustomerId { get; set; }
+        public int? CompanyId { get; set; }
+        public DateTime? LogDateTime { get; set; }
+        public string LogContent { get; set; }
+    }
+}
